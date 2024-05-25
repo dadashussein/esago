3a224f5eaf8cd1f8cbc9c5f8234a171dd4cdc533
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-05-25 15:53:53 +0400

    add origin

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,9 +5,27 @@ from routers.AuthRouter import router as user_router
 from routers.ResumeRouter import router as resume_router
 from models.BaseModel import Base
 from config.database import Engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 Base.metadata.create_all(bind=Engine)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+
 
 app.include_router(user_router, prefix="/users", tags=["Users"])
 app.include_router(resume_router, prefix="/resumes", tags=["Resumes"])
