807452e294bc6dbc8405858de6ed78c5fd21ec10
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-06-05 13:28:53 +0400

    Update mapper.py

--- backend/src/config/mapper.py
+++ backend/src/config/mapper.py
@@ -10,13 +10,10 @@ def map_schema_to_model(schema_instance: BaseModel, model_class: Type) -> Any:
     :param model_class: The model class to map to.
     :return: An instance of the model class with attributes mapped from the schema instance.
     """
-    # Convert the schema instance to a dictionary
     schema_dict = asdict(schema_instance)
 
-    # Initialize an instance of the model class
     model_instance = model_class()
 
-    # Map attributes from the schema to the model
     for field, value in schema_dict.items():
         if hasattr(model_instance, field):
             setattr(model_instance, field, value)
