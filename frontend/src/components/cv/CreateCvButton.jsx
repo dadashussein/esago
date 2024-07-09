4457dfa498b7dd7d04280fcfbe1cee0b4ae497b3
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-07-10 03:17:21 +0400

    fixed for safari

--- frontend/src/components/cv/CreateCvButton.jsx
+++ frontend/src/components/cv/CreateCvButton.jsx
@@ -1,48 +1,40 @@
-import Tilt from "react-next-tilt";
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import { useState } from "react";
 import { Plus } from "lucide-react";
+import { Minus } from "lucide-react";
 
 const CreateCvButton = ({ cvTitle, handleInputChange, handleCreate }) => {
-  return (
-    <DropdownMenu.Root>
-      <Tilt>
-        <div
-          className="w-[202px] h-[272px]
-                 rounded flex items-center border 
-                  justify-center bg-white/40 backdrop-blur-xl dark:bg-darkColor-hover "
-        >
-          <DropdownMenu.Trigger asChild>
-            <div className="cursor-pointer ">
-              <Plus size={32} />
-            </div>
-          </DropdownMenu.Trigger>
-        </div>
+  const [open, setOpen] = useState(false);
 
-        <DropdownMenu.Portal>
-          <DropdownMenu.Content>
-            <div className="mt-[56px]  flex flex-col w-[11.2rem]">
-              <input
-                type="text"
-                className="input-primary"
-                placeholder="Enter CV title"
-                value={cvTitle}
-                onChange={handleInputChange}
-              />
-              <button
-                onClick={handleCreate}
-                className="border
-                             text-sm  bg-white
-                             dark:border-none
-                             dark:bg-darkColor-bg dark:text-darkColor-text
-                             "
-              >
-                Create
-              </button>
-            </div>
-          </DropdownMenu.Content>
-        </DropdownMenu.Portal>
-      </Tilt>
-    </DropdownMenu.Root>
+  return (
+    <div className="relative flex items-center justify-center ">
+      <div
+        className="w-[202px] h-[272px]
+                 rounded flex items-center border 
+                 justify-center bg-white/40 backdrop-blur-xl dark:bg-darkColor-hover "
+      >
+        <div className="cursor-pointer" onClick={() => setOpen(!open)}>
+          {open ? <Minus size={32} /> : <Plus size={32} />}
+        </div>
+      </div>
+
+      {open && (
+        <div className="absolute w-full items-center justify-center flex flex-col bottom-0 h-[5rem] bg-black/15 px-1 py-4   backdrop-blur-2xl   gap-2">
+          <input
+            type="text"
+            className="focus:outline-none text-center  text-sm p-1 rounded"
+            placeholder="Enter CV title"
+            value={cvTitle}
+            onChange={handleInputChange}
+          />
+          <button
+            onClick={handleCreate}
+            className="py-0 px-1 text-sm bg-white rounded"
+          >
+            Create
+          </button>
+        </div>
+      )}
+    </div>
   );
 };
 
